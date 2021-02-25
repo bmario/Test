81b0a10cce6c1960af5652d18eab5ff21df0a0c5
Mario Bielert <mario.bielert@googlemail.com>
2021-02-25 13:10:12 +0100

    Update foo.cpp

--- foo.cpp
+++ foo.cpp
@@ -1,8 +1,14 @@
 #include<iostream>
 
+void help();
+
 int
 main(
   ){
   std::cout <<
           "Hello turtle, you're the best <3\n"
     ;}
+
+void help() {
+ std::cout << "Whelp!"   << std::endl; 
+}
